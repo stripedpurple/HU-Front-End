e284811034945ac55854108d67eae2426a60790a
Austin Barrett <virus.cmd@gmail.com>
2017-03-22 08:08:02 -0400

    added vehicle section

--- project 2/website/routes/index.js
+++ project 2/website/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+
+// By Vehicle
+router.get('/vehicle', function (req, res, next) {
+    res.render('vehicle');
 });
 
 module.exports = router;
